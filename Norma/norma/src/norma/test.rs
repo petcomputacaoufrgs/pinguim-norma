3a4a710140e065ecabd5acc2eed48e04495cad46
Bruno Zimmermann <brunoczim@gmail.com>
2021-07-06 22:09:53 -0300

    tests on const operations, invalid regs

--- Norma/norma/src/norma/test.rs
+++ Norma/norma/src/norma/test.rs
@@ -2,33 +2,6 @@ use super::Machine;
 use num_bigint::BigUint;
 use num_traits::identities::{One, Zero};
 
-#[test]
-fn insert() {
-    let mut machine = Machine::new(BigUint::from(4u8));
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(4u8)
-    );
-
-    machine.insert("A");
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::zero());
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(4u8)
-    );
-
-    machine.insert_with_value("B", BigUint::from(13u8));
-    assert_eq!(
-        machine.get_register("B").unwrap().get_value(),
-        BigUint::from(13u8)
-    );
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::zero());
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(4u8)
-    );
-}
-
 fn make_machine() -> Machine {
     let mut machine = Machine::new(BigUint::from(4u8));
     machine.insert("A");
@@ -38,83 +11,69 @@ fn make_machine() -> Machine {
 }
 
 #[test]
+fn insert() {
+    let mut machine = Machine::new(BigUint::from(4u8));
+    assert_eq!(machine.get_value("X"), BigUint::from(4u8));
+
+    machine.insert("A");
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(4u8));
+
+    machine.insert_with_value("B", BigUint::from(13u8));
+    assert_eq!(machine.get_value("B"), BigUint::from(13u8));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(4u8));
+}
+
+#[test]
+#[should_panic]
+fn invalid_get() {
+    let mut machine = make_machine();
+    machine.get_value("K");
+}
+
+#[test]
 fn inc() {
     let mut machine = make_machine();
     machine.inc("X");
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(5u8)
-    );
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::zero());
-    assert_eq!(
-        machine.get_register("B").unwrap().get_value(),
-        BigUint::from(13u8)
-    );
-    assert_eq!(machine.get_register("Y").unwrap().get_value(), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(5u8));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(13u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
 
     machine.inc("B");
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(5u8)
-    );
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::zero());
-    assert_eq!(
-        machine.get_register("B").unwrap().get_value(),
-        BigUint::from(14u8)
-    );
-    assert_eq!(machine.get_register("Y").unwrap().get_value(), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(5u8));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(14u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
 
     machine.inc("A");
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(5u8)
-    );
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::one());
-    assert_eq!(
-        machine.get_register("B").unwrap().get_value(),
-        BigUint::from(14u8)
-    );
-    assert_eq!(machine.get_register("Y").unwrap().get_value(), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(5u8));
+    assert_eq!(machine.get_value("A"), BigUint::one());
+    assert_eq!(machine.get_value("B"), BigUint::from(14u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
 }
 
 #[test]
 fn dec() {
     let mut machine = make_machine();
     machine.dec("X");
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(3u8)
-    );
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::zero());
-    assert_eq!(
-        machine.get_register("B").unwrap().get_value(),
-        BigUint::from(13u8)
-    );
-    assert_eq!(machine.get_register("Y").unwrap().get_value(), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(3u8));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(13u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
 
     machine.dec("B");
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(3u8)
-    );
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::zero());
-    assert_eq!(
-        machine.get_register("B").unwrap().get_value(),
-        BigUint::from(12u8)
-    );
-    assert_eq!(machine.get_register("Y").unwrap().get_value(), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(3u8));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(12u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
 
     machine.dec("A");
-    assert_eq!(
-        machine.get_register("X").unwrap().get_value(),
-        BigUint::from(3u8)
-    );
-    assert_eq!(machine.get_register("A").unwrap().get_value(), BigUint::zero());
-    assert_eq!(
-        machine.get_register("B").unwrap().get_value(),
-        BigUint::from(12u8)
-    );
-    assert_eq!(machine.get_register("Y").unwrap().get_value(), BigUint::zero());
+    assert_eq!(machine.get_value("X"), BigUint::from(3u8));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(12u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
 }
 
 #[test]
@@ -138,4 +97,44 @@ fn is_zero() {
     assert!(!machine.is_zero("A").unwrap());
     assert!(!machine.is_zero("B").unwrap());
     assert!(machine.is_zero("Y").unwrap());
+}
+
+#[test]
+fn cons_sum() {
+    let mut machine = make_machine();
+    machine.cons_sum("X", 1234567890);
+    assert_eq!(machine.get_value("X"), BigUint::from(1234567894u128));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(13u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
+}
+
+#[test]
+fn cons_sub() {
+    let mut machine = make_machine();
+    machine.cons_sub("X", 2);
+    assert_eq!(machine.get_value("X"), BigUint::from(2u8));
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(13u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
+
+    machine.cons_sub("X", 1234567890);
+    assert_eq!(machine.get_value("X"), BigUint::zero());
+    assert_eq!(machine.get_value("A"), BigUint::zero());
+    assert_eq!(machine.get_value("B"), BigUint::from(13u8));
+    assert_eq!(machine.get_value("Y"), BigUint::zero());
+}
+
+#[test]
+fn cons_cmp() {
+    let mut machine = make_machine();
+    assert!(machine.cons_cmp("X", 4).unwrap());
+    assert!(machine.cons_cmp("A", 0).unwrap());
+    assert!(machine.cons_cmp("B", 13).unwrap());
+    assert!(machine.cons_cmp("Y", 0).unwrap());
+
+    assert!(!machine.cons_cmp("X", 0).unwrap());
+    assert!(!machine.cons_cmp("A", 1).unwrap());
+    assert!(!machine.cons_cmp("B", 12).unwrap());
+    assert!(!machine.cons_cmp("Y", 9).unwrap());
 }
