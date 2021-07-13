71a765519c6498ca679741735a622849326521f7
Heric Rodrigues <heric.rodrigues@inf.ufrgs.br>
2021-07-13 20:18:20 -0300

    Added syntax highlight and other functions
    Syntax highlight working but not perfect. CSS bugs need to be fixed. Some elements got behind the texarea. Added tab ident on textarea and autocomplete for brackets and curly brackets. If the caret is inside the curly brackets it will break two lines and use tab to ident.

--- Norma/script.js
+++ Norma/script.js
@@ -1,3 +1,4 @@
+// Theme
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 function switchTheme(e) {
     if (e.target.checked) {
@@ -18,8 +19,93 @@ if (currentTheme) {
     }
 }
 
+// Upload and download buttons
 const actualBtn = document.getElementById('upload_button');
 const fileChosen = document.getElementById('file-chosen');
 actualBtn.addEventListener('change', function(){
   fileChosen.textContent = this.files[0].name
 })
+
+// Highlight 
+const textAreaHTML = document.getElementById('userinput');
+const codeAreaHTML = document.getElementById('codeholder');
+
+const reservedWords = /main|if|then|else|do|goto|operation|test/g;
+const builtInFuncs = /inc|dec|zero|add|sub|cmp/g;
+const regexLabels = /[0-9][:]/g;
+
+const spanEnd = '</span>';
+const spanLabels = '<span class="label">';
+const spanReserved = '<span class="reserved">';
+const spanBuiltIn = '<span class="builtin">';
+
+const highlight = () => {
+    let baseText = textAreaHTML.value;
+    let finalText = baseText.replace(regexLabels, (match) => {return spanLabels + match + spanEnd});
+    finalText = finalText.replace(reservedWords, (match) => {return spanReserved + match + spanEnd});
+    finalText = finalText.replace(builtInFuncs, (match) => {return spanBuiltIn + match + spanEnd});
+
+    codeAreaHTML.innerHTML = finalText
+}
+
+// Tab key
+// Allows tab identation inside textarea
+textAreaHTML.addEventListener('keydown', (e) => {
+    if(e.key == 'Tab') {
+        e.preventDefault();
+        const start = textAreaHTML.selectionStart;
+        const end = textAreaHTML.selectionEnd;
+        
+        textAreaHTML.value = textAreaHTML.value.substring(0, start) + 
+            "\t" + textAreaHTML.value.substring(end);
+
+        textAreaHTML.selectionStart = textAreaHTML.selectionEnd = start + 1;
+    }
+});
+
+// Bracket key
+// Auto complete the bracket
+textAreaHTML.addEventListener('keydown', (e) => {
+    if(e.key == '(') {
+        e.preventDefault();
+        const start = textAreaHTML.selectionStart;
+        const end = textAreaHTML.selectionEnd;
+
+        textAreaHTML.value = textAreaHTML.value.substring(0, start) + 
+            "()" + textAreaHTML.value.substring(end);
+
+        textAreaHTML.selectionStart = textAreaHTML.selectionEnd = end + 1;
+    }
+});
+
+// Curly bracket key
+// Auto complete the curly bracket
+textAreaHTML.addEventListener('keydown', (e) => {
+    if(e.key == '{') {
+        e.preventDefault();
+        const start = textAreaHTML.selectionStart;
+        const end = textAreaHTML.selectionEnd;
+
+        textAreaHTML.value = textAreaHTML.value.substring(0, start) + 
+            "{}" + textAreaHTML.value.substring(end);
+
+        textAreaHTML.selectionStart = textAreaHTML.selectionEnd = end + 1;
+    }
+});
+
+textAreaHTML.addEventListener('keydown', (e) => {
+    if(e.key == 'Enter') {
+        if((textAreaHTML.value[textAreaHTML.selectionStart - 1] == '{') && 
+            (textAreaHTML.value[textAreaHTML.selectionStart] == '}')) {
+            e.preventDefault();
+            const start = textAreaHTML.selectionStart;
+            const end = textAreaHTML.selectionEnd;
+
+            textAreaHTML.value = textAreaHTML.value.substring(0, start) +
+                "\n\t\n" + textAreaHTML.value.substring(end);
+
+            textAreaHTML.selectionStart = textAreaHTML.selectionEnd = start + 2;
+        }
+    }
+});
+
