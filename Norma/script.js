fa9d426f12ee354df5e94efce454e3c6c9d69f3b
Heric Rodrigues <heric.rodrigues@inf.ufrgs.br>
2021-07-20 21:35:31 -0300

    Changed the theme switch button
    Added sun and moon representations

--- Norma/script.js
+++ Norma/script.js
@@ -1,11 +1,14 @@
 // Theme
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        toggleIcon.innerHTML = 'dark_mode';
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        toggleIcon.innerHTML = 'light_mode';
     }    
 }
 toggleSwitch.addEventListener('change', switchTheme, false);
