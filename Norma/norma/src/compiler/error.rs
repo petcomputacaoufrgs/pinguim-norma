e4c2d0c09b7e7a3b4f8ca1f8363e27e6020e0094
Nicolle Favero <nicollepf@gmail.com>
2021-09-13 22:55:25 -0300

    handling errors and adding diagnostic stuff

--- Norma/norma/src/compiler/error.rs
+++ Norma/norma/src/compiler/error.rs
@@ -1,10 +1,10 @@
-use crate::compiler::token::Span;
+use crate::compiler::token::{Span, TokenType};
 use std::{error::Error as StdError, fmt, slice, vec};
 
 #[derive(Debug)]
 pub struct Error {
     cause: Box<dyn StdError + Send + Sync>,
-    span: Span,
+    span: Option<Span>,
 }
 
 impl Error {
@@ -12,10 +12,17 @@ impl Error {
     where
         E: StdError + Send + Sync + 'static,
     {
-        Self { cause: Box::new(cause), span }
+        Self { cause: Box::new(cause), span: Some(span) }
     }
 
-    pub fn span(&self) -> Span {
+    pub fn with_no_span<E>(cause: E) -> Self
+    where
+        E: StdError + Send + Sync + 'static,
+    {
+        Self { cause: Box::new(cause), span: None }
+    }
+
+    pub fn span(&self) -> Option<Span> {
         self.span
     }
 
@@ -26,7 +33,11 @@ impl Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "{}, {}", self.cause, self.span)
+        write!(formatter, "{}", self.cause)?;
+        if let Some(span) = self.span() {
+            write!(formatter, ", {}", span)?;
+        }
+        Ok(())
     }
 }
 
@@ -145,3 +156,92 @@ impl fmt::Display for BadCommentStart {
 }
 
 impl StdError for BadCommentStart {}
+
+// erros do parser
+#[derive(Clone, Debug)]
+pub struct MainAlreadyDeclared;
+
+impl fmt::Display for MainAlreadyDeclared {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Main já foi declarada neste programa")
+    }
+}
+
+impl StdError for MainAlreadyDeclared {}
+
+#[derive(Clone, Debug)]
+pub struct MainNotDeclared;
+
+impl fmt::Display for MainNotDeclared {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Main não foi declarada neste programa")
+    }
+}
+
+impl StdError for MainNotDeclared {}
+
+#[derive(Clone, Debug)]
+pub struct MacroAlreadyDeclared {
+    pub macro_name: String
+}
+
+impl fmt::Display for MacroAlreadyDeclared {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Macro \"{}\" já foi declarada neste programa", self.macro_name)
+    }
+}
+
+impl StdError for MacroAlreadyDeclared {}
+
+#[derive(Clone, Debug)]
+pub struct LabelAlreadyDeclared {
+    pub label_name: String
+}
+
+impl fmt::Display for LabelAlreadyDeclared {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Macro \"{}\" já foi declarada neste programa", self.label_name)
+    }
+}
+
+impl StdError for LabelAlreadyDeclared {}
+
+#[derive(Clone, Debug)]
+pub struct UnexpectedToken {
+    pub expected_types: Vec<TokenType>
+}
+
+impl fmt::Display for UnexpectedToken {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Token inesperado encontrado, esperava-se um ")?;
+
+        let count = self.expected_types.len();
+
+        if count > 1 {
+            for expected_type in &self.expected_types[..count - 2] {
+                write!(formatter, "\"{}\", ", expected_type)?;
+            }
+
+            write!(formatter, "\"{}\" ou ", self.expected_types[count-2])?;
+        }
+
+        if count > 0 {
+            write!(formatter, "\"{}\"", self.expected_types[count-1])?;
+        }
+
+        Ok(())
+    }
+}
+
+impl StdError for UnexpectedToken {}
+
+#[derive(Clone, Debug)]
+pub struct UnexpectedEndOfInput;
+
+impl fmt::Display for UnexpectedEndOfInput {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Fim inesperado do código")
+    }
+}
+
+impl StdError for UnexpectedEndOfInput {}
--- Norma/norma/src/compiler/parser.rs
+++ Norma/norma/src/compiler/parser.rs
@@ -5,14 +5,16 @@ use crate::compiler::{ast::*, token::*};
 use indexmap::IndexMap;
 use num_bigint::BigUint;
 use std::str::FromStr;
+use crate::compiler::error::*;
+use std::error::Error as StdError;
 
-pub fn parse(tokens: Vec<Token>) -> Result<Option<Program>, Abort> {
-    Parser::new(tokens).parse_program()
+pub fn parse(tokens: Vec<Token>, diagnostics: &mut Diagnostics) -> Result<Option<Program>, Abort> {
+    Parser::new(tokens).parse_program(diagnostics)
 }
 
 pub struct Abort;
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 struct Parser {
     tokens: Vec<Token>,
     curr_token: usize,
@@ -27,11 +29,11 @@ impl Parser {
         self.tokens.get(self.curr_token)
     }
 
-    fn require_current(&self) -> Result<&Token, Abort> {
+    fn require_current(&self, diagnostics: &mut Diagnostics) -> Result<&Token, Abort> {
         match self.current() {
             Some(token) => Ok(token),
             None => {
-                panic!("Errooou");
+                diagnostics.raise(Error::with_no_span(UnexpectedEndOfInput));
                 Err(Abort)
             }
         }
@@ -41,21 +43,21 @@ impl Parser {
         self.curr_token += 1;
     }
 
-    fn expect(&mut self, expected_type: TokenType) -> Result<(), Abort> {
-        // falta passar o diagnostics para cá depois
-        let token = self.require_current()?;
+    fn expect(&mut self, expected_type: TokenType, diagnostics: &mut Diagnostics) -> Result<(), Abort> {
+        let token = self.require_current(diagnostics)?;
 
         if token.token_type == expected_type {
             self.next();
         } else {
-            panic!("Whatever")
+            let expected_types = vec![expected_type];
+            diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
         }
 
         Ok(())
     }
 
-    fn check_expect(&mut self, expected_type: TokenType) -> Result<bool, Abort> {
-        let token = self.require_current()?;
+    fn check_expect(&mut self, expected_type: TokenType, diagnostics: &mut Diagnostics) -> Result<bool, Abort> {
+        let token = self.require_current(diagnostics)?;
 
         if token.token_type == expected_type {
             self.next();
@@ -65,7 +67,7 @@ impl Parser {
         }
     }
 
-    fn parse_program(&mut self) -> Result<Option<Program>, Abort> {
+    fn parse_program(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Program>, Abort> {
         let mut macros = IndexMap::<String, Macro>::new();
         let mut main_option: Option<Main> = None;
 
@@ -73,12 +75,12 @@ impl Parser {
             match token.token_type {
                 TokenType::Main => {
                     // conferir se existe mais de uma main
-                    main_option = self.parse_main()?;
+                    main_option = self.parse_main(diagnostics)?;
                 },
 
                 TokenType::Operation => {
                     if let Some(macro_aux) =
-                        self.parse_macro_def(MacroType::Operation)?
+                        self.parse_macro_def(MacroType::Operation, diagnostics)?
                     {
                         // conferir se ja nao esta no indexmap
                         macros
@@ -88,7 +90,7 @@ impl Parser {
 
                 TokenType::Test => {
                     if let Some(macro_aux) =
-                        self.parse_macro_def(MacroType::Test)?
+                        self.parse_macro_def(MacroType::Test, diagnostics)?
                     {
                         // conferir se ja nao esta no indexmap
                         macros
@@ -97,31 +99,35 @@ impl Parser {
                 },
 
                 _ => {
-                    panic!("Whatever")
+                    let expected_types = vec![TokenType::Main, TokenType::Operation, TokenType::Test];
+                    diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
                 }
             }
         }
+        
+        // ver se a main é vazia e colocar erro no diagnostics
 
         Ok(main_option.map(|main| Program { main, macros }))
     }
 
-    fn parse_main(&mut self) -> Result<Option<Main>, Abort> {
+    fn parse_main(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Main>, Abort> {
         self.next();
-        let instructions = self.parse_func_body()?;
+        let instructions = self.parse_func_body(diagnostics)?;
 
         Ok(Some(Main { code: instructions }))
     }
 
-    fn parse_func_body(&mut self) -> Result<IndexMap<String, Instruction>, Abort> {
-        self.expect(TokenType::OpenCurly);
+    fn parse_func_body(&mut self, diagnostics: &mut Diagnostics) -> Result<IndexMap<String, Instruction>, Abort> {
+        self.expect(TokenType::OpenCurly, diagnostics);
         let mut code = IndexMap::<String, Instruction>::new();
 
         loop {
-            let token = self.require_current()?;
+            let token = self.require_current(diagnostics)?;
             if token.token_type == TokenType::CloseCurly {
                 self.next();
                 break;
-            } else if let Some(instr) = self.parse_instr()? {
+            } else if let Some(instr) = self.parse_instr(diagnostics)? {
+
                 // fazer a verificação de label duplicado
                 code.insert(instr.label.content.clone(), instr);
             }
@@ -130,11 +136,11 @@ impl Parser {
         Ok(code)
     }
 
-    fn parse_macro_def(&mut self, macro_type: MacroType) -> Result<Option<Macro>, Abort> {
+    fn parse_macro_def(&mut self, macro_type: MacroType, diagnostics: &mut Diagnostics) -> Result<Option<Macro>, Abort> {
         self.next();
-        let name_option = self.parse_macro_name()?;
-        let parameters = self.parse_macro_def_params()?;
-        let instructions = self.parse_func_body()?;
+        let name_option = self.parse_macro_name(diagnostics)?;
+        let parameters = self.parse_macro_def_params(diagnostics)?;
+        let instructions = self.parse_func_body(diagnostics)?;
 
         Ok(name_option.map(|name| Macro {
             macro_type,
@@ -144,8 +150,8 @@ impl Parser {
         }))
     }
 
-    fn parse_macro_name(&mut self) -> Result<Option<Symbol>, Abort> {
-        let token = self.require_current()?;
+    fn parse_macro_name(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Symbol>, Abort> {
+        let token = self.require_current(diagnostics)?;
 
         if token.token_type == TokenType::Identifier {
             let macro_name = Symbol {
@@ -155,21 +161,23 @@ impl Parser {
 
             self.next();
             Ok(Some(macro_name))
+            
         } else {
-            panic!("erro");
+            let expected_types = vec![TokenType::Identifier];
+            diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
             Ok(None)
         }
     }
 
-    fn parse_macro_def_params(&mut self) -> Result<Vec<Symbol>, Abort> {
-        self.parse_list_params(Self::parse_register)
+    fn parse_macro_def_params(&mut self, diagnostics: &mut Diagnostics) -> Result<Vec<Symbol>, Abort> {
+        self.parse_list_params(Self::parse_register, diagnostics)
     }
 
-    fn parse_instr(&mut self) -> Result<Option<Instruction>, Abort> {
-        let instr_label_option = self.parse_label()?;
-        self.expect(TokenType::Colon);
+    fn parse_instr(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Instruction>, Abort> {
+        let instr_label_option = self.parse_label(diagnostics)?;
+        self.expect(TokenType::Colon, diagnostics);
 
-        let type_option = self.parse_instr_type()?;
+        let type_option = self.parse_instr_type(diagnostics)?;
 
         let zipped = instr_label_option.zip(type_option);
         let instr = zipped.map(|(label, instruction_type)| Instruction { 
@@ -180,29 +188,30 @@ impl Parser {
         Ok(instr)
     }
 
-    fn parse_instr_type(&mut self) -> Result<Option<InstructionType>, Abort> {
-        let token = self.require_current()?;
+    fn parse_instr_type(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<InstructionType>, Abort> {
+        let token = self.require_current(diagnostics)?;
         
         if token.token_type == TokenType::Do {
-            let op_option = self.parse_instr_op()?;
+            let op_option = self.parse_instr_op(diagnostics)?;
             Ok(op_option.map(|operation| InstructionType::Operation(operation)))
 
         } else if token.token_type == TokenType::If {
-            let test_option = self.parse_instr_test()?;
+            let test_option = self.parse_instr_test(diagnostics)?;
             Ok(test_option.map(|test| InstructionType::Test(test)))
 
         } else {
-            panic!("Whatever");
+            let expected_types = vec![TokenType::Do, TokenType::If];
+            diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
             Ok(None)
         }
     }
 
-    fn parse_instr_op(&mut self) -> Result<Option<Operation>, Abort> {
+    fn parse_instr_op(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Operation>, Abort> {
         self.next();
-        let oper_type = self.parse_operation_type()?;
+        let oper_type = self.parse_operation_type(diagnostics)?;
 
-        self.expect(TokenType::Goto);
-        let oper_label = self.parse_label()?;
+        self.expect(TokenType::Goto, diagnostics);
+        let oper_label = self.parse_label(diagnostics)?;
 
         let zipped = oper_type.zip(oper_label);
         let operation = zipped.map(|(oper_type, oper_label)| Operation {
@@ -213,8 +222,8 @@ impl Parser {
         Ok(operation)
     }
 
-    fn parse_operation_type(&mut self) -> Result<Option<OperationType>, Abort> {
-        let token = self.require_current()?;
+    fn parse_operation_type(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<OperationType>, Abort> {
+        let token = self.require_current(diagnostics)?;
 
         match token.token_type {
             TokenType::Identifier => {
@@ -225,35 +234,36 @@ impl Parser {
 
                 self.next();
 
-                let parameters = self.parse_macro_params()?;
+                let parameters = self.parse_macro_params(diagnostics)?;
                 Ok(Some(OperationType::Macro(macro_name, parameters)))
             },
 
             TokenType::BuiltInOper(oper) => {
                 self.next();
-                let parameter_option = self.parse_builtin_param()?;
+                let parameter_option = self.parse_builtin_param(diagnostics)?;
                 Ok(parameter_option.map(|parameter| OperationType::BuiltIn(oper, parameter)))
             },
 
             _ => {
-                panic!("Erro");
+                let expected_types = vec![TokenType::BuiltInOper(BuiltInOperation::Inc), TokenType::BuiltInOper(BuiltInOperation::Dec), TokenType::Identifier];
+                diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
                 Ok(None)
             }
         }
             
     }
 
-    fn parse_instr_test(&mut self) -> Result<Option<Test>, Abort> {
+    fn parse_instr_test(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Test>, Abort> {
         self.next();
-        let test_type = self.parse_test_type()?;
+        let test_type = self.parse_test_type(diagnostics)?;
 
-        self.expect(TokenType::Then);
-        self.expect(TokenType::Goto);
-        let then_label = self.parse_label()?;
+        self.expect(TokenType::Then, diagnostics);
+        self.expect(TokenType::Goto, diagnostics);
+        let then_label = self.parse_label(diagnostics)?;
 
-        self.expect(TokenType::Else);
-        self.expect(TokenType::Goto);
-        let else_label = self.parse_label()?;
+        self.expect(TokenType::Else, diagnostics);
+        self.expect(TokenType::Goto, diagnostics);
+        let else_label = self.parse_label(diagnostics)?;
 
         let zipped = test_type.zip(then_label).zip(else_label);
         let test = zipped.map(|((test_type, then_label), else_label)| Test {
@@ -265,8 +275,8 @@ impl Parser {
         Ok(test)
     }
 
-    fn parse_test_type(&mut self) -> Result<Option<TestType>, Abort> {
-        let token = self.require_current()?;
+    fn parse_test_type(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<TestType>, Abort> {
+        let token = self.require_current(diagnostics)?;
 
         match token.token_type {
             TokenType::Identifier => {
@@ -277,61 +287,62 @@ impl Parser {
 
                 self.next();
 
-                let parameters = self.parse_macro_params()?;
+                let parameters = self.parse_macro_params(diagnostics)?;
                 Ok(Some(TestType::Macro(macro_name, parameters)))
             },
 
             TokenType::BuiltInTest(test) => {
                 self.next();
-                let parameter_option = self.parse_builtin_param()?;
+                let parameter_option = self.parse_builtin_param(diagnostics)?;
                 Ok(parameter_option.map(|parameter| TestType::BuiltIn(test, parameter)))
             },
 
             _ => {
-                panic!("Erro");
+                let expected_types = vec![TokenType::BuiltInTest(BuiltInTest::Zero), TokenType::Identifier];
+                diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
                 Ok(None)
             }
         }
     }
 
-    fn parse_builtin_param(&mut self) -> Result<Option<Symbol>, Abort> {
-        let has_parens = self.check_expect(TokenType::OpenParen)?;
-        let parameter = self.parse_register()?;
+    fn parse_builtin_param(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Symbol>, Abort> {
+        let has_parens = self.check_expect(TokenType::OpenParen, diagnostics)?;
+        let parameter = self.parse_register(diagnostics)?;
         if has_parens {
-            self.expect(TokenType::CloseParen)?;
+            self.expect(TokenType::CloseParen, diagnostics)?;
         }
         Ok(parameter)
     }
 
-    fn parse_list_params<F, T>(&mut self, mut parse_param: F) -> Result<Vec<T>, Abort> 
+    fn parse_list_params<F, T>(&mut self, mut parse_param: F, diagnostics: &mut Diagnostics) -> Result<Vec<T>, Abort> 
     where 
-        F: FnMut(&mut Self) -> Result<Option<T>, Abort>
+        F: FnMut(&mut Self, &mut Diagnostics) -> Result<Option<T>, Abort>
     {
-        self.expect(TokenType::OpenParen);
+        self.expect(TokenType::OpenParen, diagnostics);
 
         let mut parameters = Vec::new();
         let mut needs_comma = false;
 
-        while !self.check_expect(TokenType::CloseParen)? {
+        while !self.check_expect(TokenType::CloseParen, diagnostics)? {
             if needs_comma {
                 panic!("errooooou")
             }
 
-            if let Some(parameter) = parse_param(self)? {
+            if let Some(parameter) = parse_param(self, diagnostics)? {
                 parameters.push(parameter);
-                needs_comma = !self.check_expect(TokenType::Comma)?;
+                needs_comma = !self.check_expect(TokenType::Comma, diagnostics)?;
             }
         }
 
         Ok(parameters)
     }
 
-    fn parse_macro_params(&mut self) -> Result<Vec<MacroParam>, Abort> {
-        self.parse_list_params(Self::parse_macro_param)
+    fn parse_macro_params(&mut self, diagnostics: &mut Diagnostics) -> Result<Vec<MacroParam>, Abort> {
+        self.parse_list_params(Self::parse_macro_param, diagnostics)
     }
 
-    fn parse_macro_param(&mut self) -> Result<Option<MacroParam>, Abort> {
-        let token = self.require_current()?;
+    fn parse_macro_param(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<MacroParam>, Abort> {
+        let token = self.require_current(diagnostics)?;
 
         match token.token_type {
             TokenType::Identifier => {
@@ -352,14 +363,15 @@ impl Parser {
             },
 
             _ => {
-                panic!("erro dps");
+                let expected_types = vec![TokenType::Identifier, TokenType::Number];
+                diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
                 Ok(None)
             }
         }
     }
 
-    fn parse_register(&mut self) -> Result<Option<Symbol>, Abort> {
-        let token = self.require_current()?;
+    fn parse_register(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Symbol>, Abort> {
+        let token = self.require_current(diagnostics)?;
 
         if token.token_type == TokenType::Identifier {
             let symbol = Symbol {
@@ -369,13 +381,14 @@ impl Parser {
             self.next();
             Ok(Some(symbol))
         } else {
-            panic!("erro dps");
+            let expected_types = vec![TokenType::Identifier];
+            diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
             Ok(None)
         }
     }
 
-    fn parse_label(&mut self) -> Result<Option<Symbol>, Abort> {
-        let token = self.require_current()?;
+    fn parse_label(&mut self, diagnostics: &mut Diagnostics) -> Result<Option<Symbol>, Abort> {
+        let token = self.require_current(diagnostics)?;
 
         match token.token_type {
             TokenType::Number | TokenType::Identifier => {
@@ -389,7 +402,8 @@ impl Parser {
             },
 
             _ => {
-                panic!("Whaaaatever");
+                let expected_types = vec![TokenType::Identifier, TokenType::Number];
+                diagnostics.raise(Error::new(UnexpectedToken { expected_types }, token.span));
                 Ok(None)
             }
         }
