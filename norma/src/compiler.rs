575528bd817c19cec8684b26a09dfa932dd214c7
brunoczim <brunoczim@gmail.com>
2021-10-13 21:05:00 -0300

    using non-shadowed variable names

--- norma/src/compiler.rs
+++ norma/src/compiler.rs
@@ -16,10 +16,10 @@ use parser::parse;
 pub fn compile(source: &str) -> Result<Program, Diagnostics> {
     let mut diagnostics = Diagnostics::new();
     let tokens = generate_tokens(source, &mut diagnostics);
-    let ast = parse(tokens, &mut diagnostics);
-    let maybe_program = ast.and_then(|ast| expand(&ast, &mut diagnostics));
+    let maybe_ast = parse(tokens, &mut diagnostics);
+    let maybe_prog = maybe_ast.and_then(|ast| expand(&ast, &mut diagnostics));
 
-    match maybe_program {
+    match maybe_prog {
         Some(runtime_program) if diagnostics.is_ok() => Ok(runtime_program),
         _ => Err(diagnostics),
     }
