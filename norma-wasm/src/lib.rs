b62c2749b67163b5c4f023529c8efc3a46cc7b4d
brunoczim <brunoczim@gmail.com>
2021-10-26 09:41:39 -0300

    separating norma lib from norma wasm

--- norma-wasm/src/lib.rs
+++ norma-wasm/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::{
+use norma::{
     compiler::{
         self,
         error::{Diagnostics, Error},
@@ -8,6 +8,11 @@ use crate::{
 };
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    console_error_panic_hook::set_once();
+}
 
 //
 // - Checar erros de compilação.
@@ -187,86 +192,3 @@ impl InterpreterHandle {
         JsValue::from_serde(&self.export_status(running)).unwrap()
     }
 }
-
-/*
-/*
- * Para importar funções e estruturas padrões do JS pro Rust
- */
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    pub fn log(s: JsValue);
-}
-
-/*
- * Para importar funções e estrutura de um arquivo JS pro Rust
- */
-#[wasm_bindgen(module = "/../communication.js")]
-extern "C" {}
-
-/*
- * Estruturas a serem exportadas para o JS,
- * assim como possiveis implementações de métodos
- */
-
-// Estrutura temporaria para debug
-// Essa estrutura fará o papel temporariamente do Interpretador
-#[wasm_bindgen]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Temp {
-    pub a: usize,
-}
-
-// Estrutura para armazenar uma linha indexada
-#[wasm_bindgen]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct IndexedLine {
-    label: String,
-    line: String,
-}
-
-impl IndexedLine {
-    pub fn new(label: String, line: String) -> Self {
-        Self { label, line }
-    }
-
-    pub fn from_instruction(label: String, instruction: Instruction) -> Self {
-        Self { label, line: instruction.to_string() }
-    }
-}
-
-// Estrutura para exportar valores ao JS
-#[wasm_bindgen]
-pub struct DataExporter {
-    lines: Vec<IndexedLine>,
-    interpreter: Temp,
-}
-
-impl DataExporter {
-    pub fn new(lines: Vec<IndexedLine>, interpreter: Temp) -> Self {
-        Self { lines, interpreter }
-    }
-}
-
-#[wasm_bindgen]
-impl DataExporter {
-    // Exporta Linhas de código como
-    #[wasm_bindgen(js_name = getLines)]
-    pub fn lines_as_json(&self) -> JsValue {
-        JsValue::from_serde(&self.lines).unwrap()
-    }
-
-    // Implementação futura: retornar o valor do interpretador
-    #[wasm_bindgen]
-    pub fn get_result(&mut self) -> String {
-        String::from("2021")
-    }
-
-    // Start machine
-    // #[wasm_bindgen(js_name = startMachine)]
-    // pub fn start_machine(&mut self, input: String) {
-    //     self.interpreter.reset();
-    //     self.interpreter.input(input);
-    // }
-}
-*/
--- norma/src/lib.rs
+++ norma/src/lib.rs
@@ -1,112 +1,11 @@
+pub mod compiler;
+pub mod machine;
+pub mod interpreter;
+
+use compiler::error::Diagnostics;
 use interpreter::run_once;
 use num_bigint::BigUint;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use wasm_bindgen::prelude::*;
-
-mod compiler;
-mod machine;
-mod interpreter;
-mod communication;
-
-pub use compiler::{
-    error::{Diagnostics, Error},
-    position::{Position, Span},
-};
-
-use machine::Machine;
 
 pub fn run(source: &str, input: BigUint) -> Result<BigUint, Diagnostics> {
     compiler::compile(source).map(|program| run_once(input, program))
-}
-
-// Import javascript functions
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
-
-    #[wasm_bindgen(js_namespace= console , js_name = log)]
-    fn print_wasm(a: &str);
-
-    #[wasm_bindgen(js_namespace= console , js_name = log)]
-    fn print_number(a: usize);
-}
-
-/// A Norma machine adapted to be exported for JS
-/// registers -> k: name of register, v: value
-/// counter -> machine counter
-#[derive(Serialize, Deserialize)]
-pub struct ExportableMachine {
-    pub registers: HashMap<String, String>,
-    pub counter: String,
-}
-
-impl ExportableMachine {
-    pub fn from_machine(mut machine: Machine) -> ExportableMachine {
-        return ExportableMachine {
-            registers: machine.export_registers(),
-            //counter: machine.get_counted_steps().to_str_radix(10).
-            // to_string(),
-            counter: todo!(),
-        };
-    }
-}
-
-#[wasm_bindgen]
-pub fn vetor(input: &[usize]) {
-    for &number in input.iter() {
-        print_number(number);
-    }
-}
-
-#[wasm_bindgen]
-pub fn square(input: &str) -> JsValue {
-    let mut registers =
-        Machine::new(BigUint::parse_bytes(input.as_bytes(), 10).unwrap());
-    registers.insert("VAL");
-    registers.insert("TMP");
-    registers.insert("CNT");
-
-    while !registers.is_zero("X") {
-        registers.dec("X");
-        registers.inc("VAL");
-        registers.inc("CNT");
-    }
-
-    while !registers.is_zero("CNT") {
-        registers.dec("CNT");
-
-        while !registers.is_zero("VAL") {
-            registers.inc("Y");
-            registers.inc("TMP");
-            registers.dec("VAL");
-        }
-
-        while !registers.is_zero("TMP") {
-            registers.inc("VAL");
-            registers.dec("TMP");
-        }
-    }
-
-    JsValue::from_serde(&ExportableMachine::from_machine(registers)).unwrap()
-}
-
-#[wasm_bindgen]
-pub fn test(input: &str) -> JsValue {
-    let mut registers =
-        Machine::new(BigUint::parse_bytes(input.as_bytes(), 10).unwrap());
-    registers.insert("J");
-    for _i in 1 ..= 3 {
-        registers.dec("X");
-        registers.inc("X");
-        registers.inc("J");
-        registers.inc("J");
-    }
-
-    if registers.is_zero("X") {
-        registers.inc("Y");
-        registers.dec("J");
-    }
-
-    JsValue::from_serde(&ExportableMachine::from_machine(registers)).unwrap()
 }
